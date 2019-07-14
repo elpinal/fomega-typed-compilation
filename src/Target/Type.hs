1539b93aee44083ecc906b226095eeedd57b0235
elpinal <elpinal@users.noreply.github.com>
2019-07-14 20:23:38 +0900

    Define `As` to realize which types actually existential types denote

--- src/Target/Type.hs
+++ src/Target/Type.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE FlexibleInstances #-}
+
 module Target.Type
   ( BSym(..)
   , TSym(..)
@@ -11,3 +14,28 @@ class BSym repr where
 
 class BSym repr => TSym repr where
   tarrow :: repr a -> repr b -> repr (a -> b)
+
+newtype Equality a b = Equality { getEquality :: forall c. c a -> c b }
+
+refl :: Equality a a
+refl = Equality id
+
+newtype As t a = As (Maybe (Equality a t))
+
+instance BSym (As Int) where
+  tint    = As $ return refl
+  tbool   = As Nothing
+  tstring = As Nothing
+  tunit   = As Nothing
+
+instance TSym (As Int) where
+  tarrow _ _ = As Nothing
+
+instance BSym (As String) where
+  tint    = As Nothing
+  tbool   = As Nothing
+  tstring = As $ return refl
+  tunit   = As Nothing
+
+instance TSym (As String) where
+  tarrow _ _ = As Nothing
